ba378cfba632459f31e9d88c0b35ffbf979875bd
PBV <pavljukbogdan@gmail.com>
2024-09-08 22:24:09 +0300

    a link refactor

--- PetProjectPixi/src/scripts/App.ts
+++ PetProjectPixi/src/scripts/App.ts
@@ -2,10 +2,12 @@ import * as PIXI from "pixi.js"
 import {Loader} from "./Loader";
 import {MainScene} from "./MainScene";
 import {Tween, update} from "@tweenjs/tween.js";
+import {CoinAnimation} from "./CoinAnimation";
 export class App {
     private app!: PIXI.Application;
     private loader!: Loader;
     private scene!: MainScene;
+    private coinAnimation! : CoinAnimation;
     public run(): void {
         //create canvas
         this.app = new PIXI.Application({resizeTo: window});
@@ -17,7 +19,10 @@ export class App {
 
     private start(): void {
         this.app.ticker.add(() => {update()});
-        this.scene = new MainScene();
-        this.app.stage.addChild(this.scene.container);
+        // this.scene = new MainScene();
+        //this.app.stage.addChild(this.scene.container);
+
+        this.coinAnimation = new CoinAnimation(this.app);
+        this.app.stage.addChild(this.coinAnimation.container);
     }
 }
--- PetProjectPixi/src/scripts/LoaderConfig.ts
+++ PetProjectPixi/src/scripts/LoaderConfig.ts
@@ -12,6 +12,14 @@ export const LoaderConfig: ILoaderConfig = {
 
     click: require("../sounds/click.mp3"),
     music: require("../sounds/music.mp3"),
+
+    scatter_0: require("../sprites/scatter_0.png"),
+    scatter_1: require("../sprites/scatter_1.png"),
+    scatter_2: require("../sprites/scatter_2.png"),
+    scatter_3: require("../sprites/scatter_3.png"),
+    scatter_4: require("../sprites/scatter_4.png"),
+    scatter_5: require("../sprites/scatter_5.png"),
+    scatter_6: require("../sprites/scatter_6.png"),
 };
 
 export interface ILoaderConfig {
